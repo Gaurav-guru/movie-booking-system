4cf0c819bef0a3533a3e8ae382c78dfd408bf04c
gaurav-patil-hub <112756896+gaurav-patil-hub@users.noreply.github.com>
2024-07-02 19:25:29 +0530

    frontend
    frontend commited

---
+++ Client/src/components/Home.js
@@ -0,0 +1,16 @@
+import Header from "./Header";
+import MovieList from "./Movielist";
+function Home() {
+  
+
+  return (
+    <div>
+      <Header/>
+      <MovieList/>
+s
+      
+    </div>
+  );
+}
+
+export default Home;
---
+++ Client/src/routes/route.js
@@ -0,0 +1,8 @@
+
+
+export const routePath = {
+    login      : '/login',
+    register      : '/register',
+    invalid     : '/*' 
+}
+  //exporting the routePath object to be used in other components.
