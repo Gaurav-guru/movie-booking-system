72a874143924cd741b0fbccb6fd63b1e7d1b13ee
gaurav-patil-hub <112756896+gaurav-patil-hub@users.noreply.github.com>
2024-07-25 15:58:08 +0530

    front unnneccessary

--- Client/src/routes/route.js
+++
@@ -1,8 +0,0 @@
-
-
-export const routePath = {
-    login      : '/login',
-    register      : '/register',
-    invalid     : '/*' 
-}
-  //exporting the routePath object to be used in other components.
